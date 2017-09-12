16c9fec5605d31fb00956efe66c2584d8fca53f5
fennec-fox <fennec.fox@titicaca-corp.com>
2017-09-12 18:47:49 +0900

    WIP:init spring boot

---
+++ osori-admin/src/main/kotlin/com/osori/admin/OsoriAdminApplication.kt
@@ -0,0 +1,12 @@
+package com.osori.admin
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+
+@SpringBootApplication
+class OsoriAdminApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(OsoriAdminApplication::class.java, *args)
+}
