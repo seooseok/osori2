58329e3865cab2d4680d84dc74970d50f2644853
5dolstory <elijah17@gmail.com>
2017-11-07 11:01:12 +0900

    feat: search user hateoas (#5)

--- osori-cave/src/main/kotlin/com/osori/cave/user/controller/UserController.kt
+++ osori-cave/src/main/kotlin/com/osori/cave/user/controller/UserController.kt
@@ -1,11 +1,17 @@
 package com.osori.cave.user.controller
 
+import com.fasterxml.jackson.annotation.JsonView
 import com.osori.cave.user.UserSearchCondition
 import com.osori.cave.user.UserService
 import com.osori.cave.user.infrastructure.User
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.format.annotation.DateTimeFormat
+import org.springframework.hateoas.Resource
+import org.springframework.hateoas.Resources
+import org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo
+import org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -17,17 +23,30 @@ class UserController
 @Autowired constructor(private val userService: UserService) {
 
 
+    @JsonView(UserView.Base::class)
     @GetMapping("/users")
     fun search(@DateTimeFormat(pattern = "yyyy-MM-dd") @RequestParam startDate: LocalDate,
                @DateTimeFormat(pattern = "yyyy-MM-dd") @RequestParam endDate: LocalDate,
-               loginId: String? = null,
-               name: String? = null,
-               status: String? = null): List<UserResource> {
+               @RequestParam(required = false) loginId: String? = null,
+               @RequestParam(required = false) name: String? = null,
+               @RequestParam(required = false) status: String? = null): Resources<UserResource> {
 
         var enumStatus: User.Status? = null
 
         status?.let { enumStatus = User.Status.valueOf(it.toUpperCase()) }
 
-        return userService.findUsers(UserSearchCondition(startDate, endDate, loginId, name, enumStatus))
+        val resources = userService.findUsers(UserSearchCondition(startDate, endDate, loginId, name, enumStatus))
+
+        resources.forEach { it.add(linkTo(methodOn(this::class.java).findOneWithDetail(it.id!!)).withRel("detail")) }
+
+        return Resources(resources, linkTo(methodOn(this::class.java).search(startDate, endDate, loginId, name, status)).withSelfRel())
     }
+
+    @JsonView(UserView.Detail::class)
+    @GetMapping("/user/{id}")
+    fun findOneWithDetail(@PathVariable id: Long): Resource<UserResource> {
+        val resource = userService.findOne(id)
+        return Resource(resource, linkTo(methodOn(this::class.java).findOneWithDetail(id)).withSelfRel())
+    }
+
 }
