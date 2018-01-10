9c5f1e09309a7d6d35d4b463dd0143edb6645768
fennec-fox <fennec.fox@titicaca-corp.com>
2018-01-10 19:21:38 +0900

    refactor: jackson config

--- osori-cave/src/main/kotlin/com/osori/cave/utils/Jackson.kt
+++ osori-cave/src/main/kotlin/com/osori/cave/utils/Jackson.kt
@@ -1,7 +1,10 @@
 package com.osori.cave.utils
 
+import com.fasterxml.jackson.databind.MapperFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 
 
@@ -10,7 +13,10 @@ class Jackson {
         fun getMapper(): ObjectMapper {
             return Jackson2ObjectMapperBuilder.json()
                     .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                    .modules(JavaTimeModule())
                     .build<ObjectMapper>()
+                    .configure(MapperFeature.DEFAULT_VIEW_INCLUSION, true)
+                    .registerKotlinModule()
         }
     }
 }
